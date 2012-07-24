222fbf4e7077bd105cc0d624c3d512402cd4c2d2
zsan <zaksantanu@gmail.com>
2012-07-24 13:37:15 +0700

    initial commit

---
+++ car_for_sale/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarForSale::Application.config.secret_token = '4595e69776a50bf68ff36a43e78be692232d1f0aedda55c9f5ffdc36448a692b00722a16c9e2c5335800b076e0a5834858347e0b61ac509f74dcb659ef9d49fb'
